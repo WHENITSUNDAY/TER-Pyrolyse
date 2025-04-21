b5afeba471548834808bded3ba9fa8d33662ff39
WHENITSUNDAY <julien.leger@bordeaux-inp.fr>
2025-04-21 23:02:12 +0200

    Regroupement du code autour de la procédure save_temp

--- couplage.f90
+++ couplage.f90
@@ -6,126 +6,109 @@ program couplage
 
     implicit none
 
-    real(PR), dimension(3)  :: k, k_new
-    real(PR)                :: t, t0, tf, dt, dx, T_0, T_init, T_Nx, khi, mv_bois, lambda_phalf, lambda_mhalf, eta, L
-    real(PR), dimension(:), allocatable :: T_new, T_n, rho_b, rho_c, rho_g, rho_l, rho_v, rhoCp, lambda
-    integer                 :: type_bois, Nx, Nt, n, i, counter
-    character(len=20)           :: nc
+    real(PR)                :: tf, L
+    integer                 :: type_bois, Nx, schema
     
     
-
-    type_bois = 1
-    tf = 1._PR
-    t0 = 0._PR
-    T_0 = 800._PR
-    T_init = 300._PR
-    T_Nx = 800._PR
-    eta = 0._PR
     L = 0.1_PR
+    Nx = 64
+    tf = 500
+    type_bois = 1
+    schema = 1
 
-
-    Nx = 100
-    dx = L/Nx
-    
-
-    khi = humidite(type_bois)
-    mv_bois = densite_bois(type_bois)
-
-    allocate(rho_b(0:Nx), rho_c(0:Nx), rho_g(0:Nx), rho_l(0:Nx), rho_v(0:Nx), T_n(0:Nx), T_new(0:Nx), rhoCp(0:Nx), lambda(0:Nx))
-
-    rho_b = mv_bois*(1._PR-khi)
-    rho_c = 0._PR
-    rho_g = 0._PR
-    rho_l = mv_bois*khi
-    rho_v = 0._PR
-
-    Nt = 2000
-    dt = (tf-t0)/Nt
-
-    T_n = T_init
-    T_new = T_init
-    T_n(0) = T_0
-    T_n(Nx) = T_Nx
-    T_new(0) = T_0
-    T_new(Nx) = T_Nx
-
-    counter = 0
-
-    do n = 0, Nt
-
-        print *, t, T_new(5)
-        
-
-        if ( mod(n, 100) == 0) then
-
-    
-                
-            write(nc,*) counter
-            open(unit = 100, file='data_temp/temperature_t'//trim(adjustl(nc))//'.dat')
-            open(unit = 101, file='data_temp/rho_t'//trim(adjustl(nc))//'.dat')
-
-            counter = counter +1
-        
-        end if
-        
-        do i = 0, Nx
-
-            rhoCp(i) = rho_b(i)*Cp(1) + rho_c(i)*Cp(2) + rho_g(i)*Cp(3) + rho_l(i)*Cp(4) + rho_v(i)*Cp(5)
-
-            eta = rho_b(i)/(rho_b(i)+rho_c(i))
-            lambda(i) = eta*0.105_PR + (1-eta)*(0.166_PR + 0.369_PR*khi)
-        end do
-
-        
-        write(100,*), dx*0, T_new(0)
-        do i = 1, Nx-1
-            
-            lambda_mhalf = (2 * lambda(i-1)* lambda(i))/(lambda(i+1) + lambda(i))
-            lambda_phalf = (2 * lambda(i+1) * lambda(i))/(lambda(i+1) + lambda(i))
-            T_new(i) = T_n(i) + dt/(dx**2 * rhoCp(i)) * &
-            (lambda_phalf*T_n(i+1) - (lambda_phalf + lambda_mhalf) * T_n(i) + lambda_mhalf * T_n(i-1))  
-            
-            if ( mod(n, 100) == 0) then
-                
-                write(100,*), dx*i, T_new(i)
-            
-            end if
-        end do
-
-        write(100,*) dx*Nx, T_new(Nx)
-
-        
-        do i = 0, Nx
-
-            call arrhenius(k, T_n(i))
-            call arrhenius(k_new, T_new(i))
-            call CK2_step(rho_b(i), rho_c(i), rho_g(i), rho_l(i), rho_v(i), k, k_new, dt)
-
-            write(101,*), dx*i, rho_b(i), rho_c(i), rho_g(i), rho_l(i), rho_v(i)
-
-        end do
-
-        T_n = T_new
-        t = t + dt
-
-        close(100)
-    end do 
-
-    deallocate(rho_b, rho_c, rho_g, rho_l, rho_v, T_n, T_new, rhoCp, lambda)
     contains
 
 
-        subroutine arrhenius(k, Temp)
+        subroutine save_temp(Nx, tf, type_bois, freq, schema)
+            integer, intent(in)             :: Nx, type_bois, freq, schema 
+            real(PR), intent(in)            :: tf
+            real(PR)                        :: tn, dt, dx, eta, khi, mv_bois
+            real(PR), dimension(0:Nx+1)     :: T, Tnew , rho_b, rho_c, rho_g, rho_l, rho_v, rhoCp, lambda
+            real(PR), dimension(Nx)         :: diag
+            real(PR), dimension(Nx-1)       :: udiag
+            real(PR), dimension(Nx, Nx)     :: A, Id
+            real(PR), dimension(3)          :: k, knew
+            integer                         :: i, ct
+            character(len=20)               :: tn_str, sch_str
 
-            real(PR), dimension(:), intent(out) :: k
-            real(PR), intent(in)                :: Temp
-            integer                             :: i
+            
 
-            k = (/(A(i)*exp(-E(i)/(R*Temp)), i = 1, 3)/)
+            !Initialisation Schéma
 
-        end subroutine arrhenius
+            tn = 0._PR
+            ct = 0
+            dx = 1._PR/(Nx+1)
+            Id = 0._PR
 
-        ! subroutine calcul_rhoCp(rhoCp)
+            do i = 1, Nx
+                Id(i,i) = 1._PR
+            end do
+            !Initialisation Température
+            T(0) = 800._PR
 
-        ! end subroutine calcul_rhoCp
+            do i = 1, Nx+1
+                T(i) = 300._PR !K
+            end do
+
+            !Initialisation Chimie
+            khi = humidite(type_bois)
+            mv_bois = densite_bois(type_bois)
+            rho_b = mv_bois*(1._PR-khi)
+            rho_c = 0._PR
+            rho_g = 0._PR
+            rho_l = mv_bois*khi
+            rho_v = 0._PR
+            
+            do while (tn < tf)
+
+                if ( mod(ct, freq) == 0) then
+
+                    write(tn_str,'(F10.2)') tn
+                    write(sch_str,*) schema
+                    open(unit = 100, file='data/temp_t'//trim(adjustl(tn_str))//'_sch'//trim(adjustl(sch_str))//'.dat')
+ 
+                end if
+                
+                do i = 0, Nx+1
+                    rhoCp(i) = rho_b(i)*Cp(1) + rho_c(i)*Cp(2) + rho_g(i)*Cp(3) + rho_l(i)*Cp(4) + rho_v(i)*Cp(5)
+                    eta = rho_b(i) / (rho_b(i) + rho_c(i))
+                    lambda(i) = eta * 0.105_PR + (1.0_PR - eta) * (0.166_PR + 0.369_PR * khi)
+                end do
+
+                call remplissage_A(A, Nx, lambda, rhoCp, dx, dt)
+
+                !Résolution du système linéaire en fonction du schéma
+                select case (schema)
+
+                    case (1) !Euler Explicite
+                        
+                    case (2) !Euler Implicite
+
+                    case (3) !Crank-Nicolson
+
+                end select 
+
+
+                do i = 0, Nx+1
+
+                    call arrhenius(k, T(i))
+                    call arrhenius(knew, Tnew(i))
+                    call CK2_step(rho_b(i), rho_c(i), rho_g(i), rho_l(i), rho_v(i), k, knew, dt)
+
+                    if ( mod(ct, freq) == 0) then
+
+                        write(101,*) dx*i, Tnew(i), rho_b(i), rho_c(i), rho_g(i), rho_l(i), rho_v(i)
+
+                    end if
+
+                end do
+
+                T = Tnew
+                t = t + dt
+
+                ct = ct + 1
+
+                close(100)
+            end do 
+        end subroutine save_temp
 end program
