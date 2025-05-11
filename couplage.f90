718f62d08e1cb93edb1e680c5a824225521f2e81
WHENITSUNDAY <julien.leger@bordeaux-inp.fr>
2025-05-11 21:32:14 +0200

    Commit final : Dernière modif et clôture du TER

--- couplage.f90
+++ couplage.f90
@@ -18,29 +18,29 @@ program couplage
     !ny=128, tmax=200._PR, type_bois_1=1, type_bois_2=5, cfl=2._PR, freq=100)
 
     
-    T= 0._PR
-    T_f = 0._PR
-    Nx = 512
-    cfl0 = 0.02_PR
-    call save_vect_temp(schema=2, T=T, ci=1, cl=1, L=0.2_PR, imax=Nx, tmax=20._PR, type_bois=1, cfl=cfl0)
+    ! T= 0._PR
+    ! T_f = 0._PR
+    ! Nx = 512
+    ! cfl0 = 0.02_PR
+    ! call save_vect_temp(schema=2, T=T, ci=1, cl=1, L=0.2_PR, imax=Nx, tmax=20._PR, type_bois=1, cfl=cfl0)
 
-    print *, "Euler Implicite"
-    do i = 1, 2
-        T_f = 0._PR
-        cfl = cfl0/(2**i)
-        call save_vect_temp(schema=2, T=T_f, ci=1, cl=1, L=0.2_PR, imax=Nx, tmax=20._PR, type_bois=1, cfl=cfl)
-        call calcul_L2_err(T, T_f, 2, Nx, cfl)
-    end do
+    ! print *, "Euler Implicite"
+    ! do i = 1, 2
+    !     T_f = 0._PR
+    !     cfl = cfl0/(2**i)
+    !     call save_vect_temp(schema=2, T=T_f, ci=1, cl=1, L=0.2_PR, imax=Nx, tmax=20._PR, type_bois=1, cfl=cfl)
+    !     call calcul_L2_err(T, T_f, 2, Nx, cfl)
+    ! end do
     
-    print *, '------------------'
-    print *, "Crank Nicolson"
-    call save_vect_temp(schema=3, T=T, ci=1, cl=1, L=0.2_PR, imax=Nx, tmax=20._PR, type_bois=1, cfl=cfl0)
-    do i = 1, 2
-        T_f = 0._PR
-        cfl = cfl0/(2**i)
-        call save_vect_temp(schema=3, T=T_f, ci=1, cl=1, L=0.2_PR, imax=Nx, tmax=20._PR, type_bois=1, cfl=cfl)
-        call calcul_L2_err(T, T_f, 3, Nx, cfl)
-    end do
+    ! print *, '------------------'
+    ! print *, "Crank Nicolson"
+    ! call save_vect_temp(schema=3, T=T, ci=1, cl=1, L=0.2_PR, imax=Nx, tmax=20._PR, type_bois=1, cfl=cfl0)
+    ! do i = 1, 2
+    !     T_f = 0._PR
+    !     cfl = cfl0/(2**i)
+    !     call save_vect_temp(schema=3, T=T_f, ci=1, cl=1, L=0.2_PR, imax=Nx, tmax=20._PR, type_bois=1, cfl=cfl)
+    !     call calcul_L2_err(T, T_f, 3, Nx, cfl)
+    ! end do
 
     contains 
 
--- readme.md
+++ readme.md
@@ -2,3 +2,8 @@
 1D, CK, Temps d'exécution :  0.33322200000000002     
 2D, EI, Temps d'exécution :   305.60074700000001
 2D_2W, EI, Temps d'exécution :   299.80106799999999 
+
+
+![Temp & rho 1D](images/temp_rho.gif)
+![Temp & rho 2D](images/temp_rho_2D.gif)
+![Temp 2D & 2W](images/temp_2D_2W.gif)
